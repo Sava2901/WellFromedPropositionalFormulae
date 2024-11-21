32759b0c3d62caeca6612cdc3fa9854bf495c667
Sava2901 <savasergiu2006@gmail.com>
2024-11-21 23:28:51 +0200

    simplified verification for parenthesis

--- wff.py
+++ wff.py
@@ -10,11 +10,14 @@ def convert_from_relaxed(prop, is_strong=False, need_print=True):
     return prop
 
 
+
+
 class ShuntingYardConverter:
     def __init__(self, expression, need_print=False):
         self.expression = expression.replace(" ", "").replace("→", "⇒")
         self.output_queue = []
         self.operator_stack = []
+        self.parenthesis = []
         self.precedence = {
             '¬': 3,  # Unary NOT
             '∧': 2,  # AND
@@ -36,44 +39,29 @@ class ShuntingYardConverter:
 
 
     def verify_integrity(self):
-        def check_parenthesis(expression):
-            open_parenthesis = []
-            pairs = []
-            for i in range(len(expression)):
-                if expression[i] == '(':
-                    open_parenthesis.append(i)
-                if expression[i] == ')':
-                    pairs.append((open_parenthesis.pop(), i))
-            for pair in pairs:
-                left, right = pair
-                if (left - 1, right + 1) in pairs:
-                    print(self.print_info)
-                    raise Exception(f"Extra set of parenthesis at positions {left-1} and {right+2}: {self.expression[left-1:right+2]}.\n{self.expression} is not a wwf.")
-
         non_matching = re.findall(r"(?![A-Z][0-9]*|¬|∧|∨|⇒|⇔|[()]|⊤|⊥).", self.expression)
         extra_parenthesis = re.findall(r"\([A-Z][0-9]*\)", self.expression)
         if self.expression == "" or self.expression is None:
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception("Empty string is not a wff.")
         if self.expression[-1] in ["∧", "∨", "⇒", "⇔", "¬"]:
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception(f"{self.expression[-1]} cannot be last in string.\n{self.expression} is not a wwf.")
         if non_matching:
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception(
                 f"Found non-matching {'character' if len(non_matching) == 1 else 'characters'} "
                 f"{', '.join(non_matching)}.\n{self.expression} is not a wff."
             )
         if self.expression.count("(") != self.expression.count(")"):
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception(f"Different number of opening and closing parenthesis.\n{self.expression} is not a wff.")
         if extra_parenthesis:
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception(
                 f"{" ".join(elem for elem in extra_parenthesis)} "
                 f"{"has" if len(extra_parenthesis) == 1 else "have"} an extra set of parenthesis.\n{self.expression} is not a wff."
             )
-        check_parenthesis(self.expression)
 
 
     def convert(self):
@@ -84,23 +72,25 @@ class ShuntingYardConverter:
         for i, token in enumerate(tokens):
             if i:
                 if (tokens[i - 1] in ["∧", "∨", "⇒", "⇔", "¬", "("] and token in ["∧", "∨", "⇒", "⇔", ")"]) or (tokens[i - 1] == ")" and token == "("):
-                    print(self.print_info)
+                    print(self.print_info, end="")
                     raise Exception(f"Invalid {"parenthesis" if tokens[i - 1] in ["(", ")"] and token in ["(", ")"] else "connectives"} placement: "
                                     f"{tokens[i-1]} cannot be followed by {token}\n{self.expression} cannot be a wff.")
                 if re.match(r"[A-Z][0-9]*|⊤|⊥", tokens[i - 1]) and re.match(r"[A-Z][0-9]*|⊤|⊥", token):
-                    print(self.print_info)
+                    print(self.print_info, end="")
                     raise Exception(f"An atomic formula '{tokens[i - 1]}' cannot be followed by another atomic formula '{token}'. "
                                     f"Expected a binary connective between.\n{self.expression} is not a wff.")
 
             if re.match(r"[A-Z][0-9]*|⊤|⊥", token):
                 self.output_queue.append(token)
             elif token == '(':
+                self.parenthesis.append(i)
                 self.operator_stack.append(token)
             elif token == ')':
+                self.parenthesis.insert(0, (self.parenthesis.pop(), i))
                 while self.operator_stack and self.operator_stack[-1] != '(':
                     self.output_queue.append(self.operator_stack.pop())
                 if len(self.operator_stack) == 0 :
-                    print(self.print_info)
+                    print(self.print_info, end="")
                     raise Exception(f"Valid expression expected before ).\n{self.expression} is not a wff.")
                 self.operator_stack.pop()
             elif self.is_operator(token):
@@ -110,6 +100,15 @@ class ShuntingYardConverter:
                          token not in self.right_associative))):
                     self.output_queue.append(self.operator_stack.pop())
                 self.operator_stack.append(token)
+
+        for pair in self.parenthesis:
+            lft, rgt = pair
+            if (lft - 1, rgt + 1) in self.parenthesis:
+                print(self.print_info, end="")
+                raise Exception(
+                    f"Extra set of parenthesis at positions {lft - 1} and {rgt + 2}: "
+                    f"{self.expression[lft - 1:rgt + 2]}.\n{self.expression} is not a wwf.")
+
         while self.operator_stack:
             self.output_queue.append(self.operator_stack.pop())
 
@@ -121,7 +120,7 @@ class ShuntingYardConverter:
             if get_node_expression(converted_formula).replace(" ", "") == self.expression:
                 self.print_info = f"Trying to convert {self.expression} to strong syntax.\n"
                 self.print_info += "The formula is already in strong syntax."
-            print(self.print_info)
+            print(self.print_info, end="")
 
         return converted_formula
 
@@ -152,7 +151,7 @@ class ShuntingYardConverter:
                     + "".join(f"\n{get_printed_tree(subtree, 2)}" for subtree in stack[1:])
             )
         if len(stack) != 1:
-            print(self.print_info)
+            print(self.print_info, end="")
             raise Exception(
                 f"Received a different number of binary connectives than expected, could not unite subtrees:\n"
                 + f"{get_printed_tree(stack[0], 2)}"
@@ -204,7 +203,7 @@ try:
 
             else:
                 convert_from_relaxed(proposition)
-                print("The string is a wwf.\n")
+                print("The string is a wwf.")
 
         except Exception as e:
             print(f"Error: {e}")
